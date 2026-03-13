ddb38588100499b9d3ab2426ad26e33d5eef129c
segadrive <47469912+segadrive@users.noreply.github.com>
2026-03-13 19:30:07 +0800

    Update diy-part1.sh

--- 24.10/diy-part1.sh
+++ 24.10/diy-part1.sh
@@ -1,51 +1,59 @@
-# 调用示例
-# UPDATE_PACKAGE "OpenAppFilter" "destan19/OpenAppFilter" "master" "" "custom_name1 custom_name2"
-# UPDATE_PACKAGE "open-app-filter" "destan19/OpenAppFilter" "master" "" "luci-app-appfilter oaf" 这样会把原有的open-app-filter，luci-app-appfilter，oaf相关组件删除，不会出现coremark错误。
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
 
-# UPDATE_PACKAGE "包名" "项目地址" "项目分支" "pkg/name，可选，pkg为从大杂烩中单独提取包名插件；name为重命名为包名"
-# UPDATE_PACKAGE "argon" "sbwml/luci-theme-argon" "openwrt-25.12"
-# UPDATE_PACKAGE "aurora" "eamonxg/luci-theme-aurora" "master"
-# UPDATE_PACKAGE "aurora-config" "eamonxg/luci-app-aurora-config" "master"
-# UPDATE_PACKAGE "kucat" "sirpdboy/luci-theme-kucat" "master"
-# UPDATE_PACKAGE "kucat-config" "sirpdboy/luci-app-kucat-config" "master"
+# istore
+git clone --depth=1 https://github.com/linkease/istore  package/istore
+git clone --depth=1 https://github.com/linkease/nas-packages  package/nas
+git clone --depth=1 https://github.com/linkease/nas-packages-luci  package/nas-luci
 
-#科学插件
-# UPDATE_PACKAGE "daed" "QiuSimons/luci-app-daed" "kix"
-# UPDATE_PACKAGE "helloworld-ssrp" "fw876/helloworld" "master"
-# UPDATE_PACKAGE "homeproxy" "VIKINGYFY/homeproxy" "main"
-# UPDATE_PACKAGE "momo" "nikkinikki-org/OpenWrt-momo" "main"
-# UPDATE_PACKAGE "nikki" "nikkinikki-org/OpenWrt-nikki" "main"
-# UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
-# UPDATE_PACKAGE "passwall" "Openwrt-Passwall/openwrt-passwall" "main" "pkg"
-# UPDATE_PACKAGE "passwall2" "Openwrt-Passwall/openwrt-passwall2" "main" "pkg"
-# UPDATE_PACKAGE "passwall-packages" "Openwrt-Passwall/openwrt-passwall-packages" "main" 
+# 科学插件
+# Passwall 
+# echo 'src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git' >> feeds.conf.default
+# echo 'src-git passwall https://github.com/Openwrt-Passwall/openwrt-passwall.git' >> feeds.conf.default
+# echo 'src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git' >> feeds.conf.default
+# OpenClash 
+echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >> feeds.conf.default
+# Nikki / Momo
+# echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git' >> feeds.conf.default
+# echo 'src-git momo https://github.com/nikkinikki-org/OpenWrt-momo.git' >> feeds.conf.default
+# Daed (指定 kix 分支)
+# echo 'src-git daed https://github.com/QiuSimons/luci-app-daed.git;kix' >> feeds.conf.default
+# Helloworld (SSR+)
+# echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
 
-# 常用工具与应用
-# UPDATE_PACKAGE "ddns-go" "sirpdboy/luci-app-ddns-go" "main"  #luci-app-ddns-go
-UPDATE_PACKAGE "netspeedtest" "sirpdboy/luci-app-netspeedtest" "master" "" "homebox speedtest"  #homebox speedtest测速
-UPDATE_PACKAGE "poweroffdevice" "sirpdboy/luci-app-poweroffdevice" "js"   #关机
-UPDATE_PACKAGE "taskplan" "sirpdboy/luci-app-taskplan" "master"    #计划任务
-UPDATE_PACKAGE "watchdog" "sirpdboy/luci-app-watchdog" "main"  #看门狗
-UPDATE_PACKAGE "netwizard" "sirpdboy/luci-app-netwizard" "main"  #网络设置向导
-UPDATE_PACKAGE "advancedplus" "sirpdboy/luci-app-advancedplus" "main"  #高级设置
-UPDATE_PACKAGE "partexp" "sirpdboy/luci-app-partexp" "main"  #分区助手
+# === 功能插件 Feeds ===
+# Lucky (大吉大利，端口转发/反向代理神器)
+echo 'src-git lucky https://github.com/gdy666/luci-app-lucky.git' >> feeds.conf.default
+# Openlist2 (可能是指 OpenList 或相关列表工具)
+echo 'src-git openlist2 https://github.com/sbwml/luci-app-openlist2.git' >> feeds.conf.default
+# Sirpdboy 的插件 (看门狗 & 计划任务)
+echo 'src-git watchdog https://github.com/sirpdboy/luci-app-watchdog.git' >> feeds.conf.default
+echo 'src-git taskplan https://github.com/sirpdboy/luci-app-taskplan.git' >> feeds.conf.default
+# Authshield (认证屏蔽/管理)
+echo 'src-git authshield https://github.com/iv7777/luci-app-authshield.git' >> feeds.conf.default
+# EasyTier (内网穿透组网)
+echo 'src-git easytier https://github.com/EasyTier/luci-app-easytier.git' >> feeds.conf.default
+# Tailscale Community (注意：改名为 tailscale_community 以防与官方 packages 里的 tailscale 混淆)
+echo 'src-git tailscale_community https://github.com/Tokisaki-Galaxy/luci-app-tailscale-community.git' >> feeds.conf.default
+# OWQ WOL (网络唤醒)
+echo 'src-git owq_wol https://github.com/isalikai/luci-app-owq-wol.git' >> feeds.conf.default
+# timecontrol (上网时间控制)
+echo 'src-git timecontrol https://github.com/gaobin89/luci-app-timecontrol.git' >> feeds.conf.default
+# modem (穿透光猫)
+echo 'src-git modem https://github.com/zyqfork/luci-app-ap-modem.git' >> feeds.conf.default
 
-# UPDATE_PACKAGE "quickfile" "sbwml/luci-app-quickfile" "main" 
-# UPDATE_PACKAGE "openlist2" "sbwml/luci-app-openlist2" "main"    #alist的新替换openlist
-UPDATE_PACKAGE "qbittorrent" "sbwml/luci-app-qbittorrent" "master" "" "qt6base qt6tools rblibtorrent"  #qbittorrent下载
-
-UPDATE_PACKAGE "authshield" "iv7777/luci-app-authshield" "main"  #防止异常登录保护
-UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-app-wolplus"
-UPDATE_PACKAGE "owq-wol" "isalikai/luci-app-owq-wol" "main"  # wol加强版
-UPDATE_PACKAGE "tailscale-community" "Tokisaki-Galaxy/luci-app-tailscale-community" "master"  #luci-app-tailscale社区版
-# UPDATE_PACKAGE "MentoHUST" "KyleRicardo/MentoHUST-OpenWrt-ipk" "master"  #锐捷验证 luci-app-mentohust
-# UPDATE_PACKAGE "diskman" "lisaac/luci-app-diskman" "master"  #luci-app-diskman
-# UPDATE_PACKAGE "easytier" "EasyTier/luci-app-easytier" "main"  #luci-app-easytier
-# UPDATE_PACKAGE "fancontrol" "rockjake/luci-app-fancontrol" "main"   #Openwrt简易通用风扇控制
-# UPDATE_PACKAGE "gecoosac" "lwb1978/openwrt-gecoosac" "main"   #集客 AC 控制器
-UPDATE_PACKAGE "open-app-filter" "destan19/OpenAppFilter" "master" "" "luci-app-appfilter oaf"  #应用过滤(OAF)
-# UPDATE_PACKAGE "lucky" "gdy666/luci-app-lucky" "main"   #lucky 大吉
-# UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
-# UPDATE_PACKAGE "qmodem" "FUjr/QModem" "main"
-UPDATE_PACKAGE "timecontrol" "gaobin89/luci-app-timecontrol" "js"  #上网时间控制
-UPDATE_PACKAGE "modem" "zyqfork/luci-app-ap-modem" "main"  #穿透光猫
+# 主题
+# git clone --depth=1 -b openwrt-25.12 https://github.com/sbwml/luci-theme-argon package/argon
+# echo 'src-git aurora https://github.com/eamonxg/luci-theme-aurora.git' >> feeds.conf.default
+# echo 'src-git aurora_config https://github.com/eamonxg/luci-app-aurora-config.git' >> feeds.conf.default
+# echo 'src-git kucat https://github.com/sirpdboy/luci-theme-kucat.git' >> feeds.conf.default
+# echo 'src-git kucat_config https://github.com/sirpdboy/luci-app-kucat-config.git' >> feeds.conf.default
